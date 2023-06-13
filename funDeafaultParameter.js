1dd050850a1e5914151b33442f02f4a32af03a98
Apurbopual131 <apurbopaul131@gmail.com>
2023-06-13 16:33:23 +0600

    added deafault parameter

---
+++ funDeafaultParameter.js
@@ -0,0 +1,3 @@
+// Declear a function with one deafault parameter.
+const defaultParameterFunc = (num1,num2=20) => num1 + num2;
+console.log(defaultParameterFunc(15));// seccond parameter take value 20
