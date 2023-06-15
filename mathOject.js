353e25fd066ce9c312ffe6161e9d4bb2b1fd3828
Apurbopual131 <apurbopaul131@gmail.com>
2023-06-15 13:03:44 +0600

    explore js math object method

---
+++ mathOject.js
@@ -0,0 +1,13 @@
+// console.log(Math.ceil(-4.2));
+// console.log(Math.floor(-4.2));
+const numbers = [10,50,77,933];
+console.log(`Max valule:${Math.max(1000,...numbers)}`);//1000
+console.log(`Min value: ${Math.min(...numbers,2)}`);//2
+//Returns the positive value
+// console.log(Math.abs(-10));
+console.log(Math.round(4.2));
+console.log(Math.round(4.4));
+console.log(Math.round(4.5));
+//Returns number between 0 to 9
+const randomNumber = Math.random() * 10; 
+console.log(randomNumber.toFixed(2));
